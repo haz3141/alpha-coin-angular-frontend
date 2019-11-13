044a5b7c10fa4071d94cec87151bbc59fde70ad2
haz3141 <38766403+haz3141@users.noreply.github.com>
2019-11-13 15:18:57 -0500

    Front-end MVP finished

---
+++ src/app/pages/create-transaction/create-transaction.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { BlockchainService } from 'src/app/services/blockchain.service';
+import { Transaction } from 'alpha-coin/src/blockchain_v1/blockchain';
+@Component({
+  selector: 'app-create-transaction',
+  templateUrl: './create-transaction.component.html',
+  styleUrls: ['./create-transaction.component.scss']
+})
+export class CreateTransactionComponent implements OnInit {
+
+  public newTx;
+  public walletKey;
+
+  constructor(private blockchainService: BlockchainService) {
+    this.walletKey = blockchainService.walletKeys[0];
+  }
+
+  ngOnInit() {
+    this.newTx = new Transaction();
+  }
+
+  createTransaction() {
+    this.newTx.timestamp = Date.now();
+    this.newTx.fromAddress = this.walletKey.publicKey;
+    this.newTx.signTransaction(this.walletKey.keyObj);
+
+    this.blockchainService.addTransaction(this.newTx);
+
+    this.newTx = new Transaction();
+  }
+
+}
